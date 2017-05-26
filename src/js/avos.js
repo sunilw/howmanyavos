70d180d1e0e16bfd7c0cc0154192f672adf94fdd
Sunil Williams <sunil@sunil.co.nz>
2017-05-27 02:14:11 +1200

    unbreak more things

--- src/js/avos.js
+++ src/js/avos.js
@@ -4,20 +4,26 @@ jQuery(document).ready(function() {
 
     //   make our datalist
     window.electorateList = [] ;
-     
+
     $(window.houseData).each(function() {
         var electorate = this.Electorate ;
         str = electorate  ;
         window.electorateList.push(str) ;
     }) ;
 
-        /**
+    /**
      * Add the datalist
      */
+    
     var electorateSelector = document.getElementById("electorateSelector");
     var awesome =  new Awesomplete(  electorateSelector  );
 
     awesome.list = electorateList ;
     
-    
+    function showStory() {
+        $("#electorateSelector").on("focus", function() { 
+            console.log("haz focus");
+        } ) ;
+    } ;
+		       
 });
