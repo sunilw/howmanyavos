26c7f6c380e0ba24e8d0d54dceec12ff3079efcb
Sunil Williams <sunil@sunil.co.nz>
2017-05-27 20:31:32 +1200

    start to add the calculations

--- js/avos.js
+++ js/avos.js
@@ -10824,18 +10824,64 @@ jQuery(document).ready(function() {
     /**
      * Add the datalist
      */
-    
     var electorateSelector = document.getElementById("electorateSelector");
     var awesome =  new Awesomplete(  electorateSelector  );
-
     awesome.list = electorateList ;
+
+    /*
+     * Placeholders for ultimate values
+     */
+    var avocadosTotal ;
+    var avocadosPerMonth ;
+    var avocadosPerWeek ;
     
+
+    /**
+     * Get the values that we need
+     */
+    var valueElectorate ;
+    var valueHousePrice ;
+    var valueAvocado ;
+
+    function getValues() {
+	valueAvocado = $("#avoPrice").val() ;
+	electorateName =  $("#electorateSelector") .val() ;
+
+	var price = window.houseData[electorateName]["House Price"] ;
+
+	// clean string: remove $
+	while(price.charAt(0) === '$') {
+	    price = price.substr(1);
+	}
+	avocadosTotal = price ;
+	// clean string: remove commas
+	price = parseFloat(price .replace( /,/g,'' )) ;	
+	valueHousePrice = price ;
+	
+        valueElectorate = document.getElementById("electorateSelector") ;
+        valueElectorate = document.getElementById("avoPrice") ;	
+	
+	/*
+	 * How many avos would we have to spare per year?
+	 */
+	avocadosTotal  =  Math.round( valueHousePrice / valueAvocado) ;	
+    }
+
+
+    function updateStoryDetails () {
+	// console.log(avocadosTotal);
+	$(".avo-total-avos").text(avocadosTotal) ;
+    } ;
+
     function showStory() {
-        console.log("got to showstory");	
+        console.log("got to showstory");
+
     } ;
 
     $("button.calculate").on("click", function(){
-	showStory() ;
+        getValues() ;
+	updateStoryDetails() ;
+        showStory() ;
     });
-    
+
 });
