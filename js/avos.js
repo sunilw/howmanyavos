0e5a8ea88d9a36f6a48ea8443f57f50986cd8f40
Sunil Williams <sunil@sunil.co.nz>
2017-05-29 14:18:02 +1200

    add validation logic

--- js/avos.js
+++ js/avos.js
@@ -10847,6 +10847,7 @@ jQuery(document).ready(function() {
      *  update the values
      */
     function getValues() {
+// <<<<<<< Updated upstream
 	valueAvocado = $("#avoPrice").val() ;
 	electorateName =  $("#electorateSelector") .val() ;
 
@@ -10861,49 +10862,104 @@ jQuery(document).ready(function() {
 	price = parseFloat(price.replace( /,/g,'' )) ;	
 	valueHousePrice = price ;
 
+// =======
+        valueAvocado = $("#avoPrice").val() ;	
+        electorateName =  $("#electorateSelector") .val() ;
+        var price = window.houseData[electorateName]["House Price"] ;
+
+        // clean house price: remove $
+        while(price.charAt(0) === '$') {
+            price = price.substr(1);
+        }
+        avocadosTotal = price ;
+// >>>>>>> Stashed changes
 	
         valueElectorate = document.getElementById("electorateSelector") ;
-        valueElectorate = document.getElementById("avoPrice") ;	
+        valueElectorate = document.getElementById("avoPrice") ;
+
+	/*
+	 * About to run calculations. Inspect figures
+	 */
+
+	console.log("About to run numbers...");
+	console.log("Avo price: ");
+	console.log(price);
 	
-	/*
-	 * How many avos would we have to spare per year?
-	 */
-	avocadosTotal  =  Math.round( valueHousePrice / valueAvocado) ;
+        /*
+         * How many avos would we have to spare per year?
+         */
+        avocadosTotal  =  Math.round( valueHousePrice / valueAvocado) ;
 
-	/*
-	 * How many avos would we have to spare per month?
-	 */
-	avocadosPerMonth = Math.round(avocadosTotal / 12) ;
-	avocadosPerWeek  = Math.round(avocadosTotal / 52) ;
+        /*
+         * How many avos would we have to spare per month?
+         */
+        avocadosPerMonth = Math.round(avocadosTotal / 12) ;
+        avocadosPerWeek  = Math.round(avocadosTotal / 52) ;
 
-	/*
-	 * How many breakfast would we have to sacrifice?
-	 */
-	var brekky = breakfastCost ;
-	lostBreakfasts = Math.round(valueHousePrice / brekky) ;
+        /*
+         * How many breakfast would we have to sacrifice?
+         */
+        var brekky = breakfastCost ;
+        lostBreakfasts = Math.round(valueHousePrice / brekky) ;
     }
 
+    /**
+     * Validation
+     */
+    function avoInputValidation() {
+        if ($("#electorateSelector").val() == '')  {
+            console.log("no electorate provided");
+            $(".error-electorateSelector").show() ;
+            $(".error-electorateSelector").css("display", "inline") ;
+            return 1 ;
+        } else {
+            console.log("electorate provided. we are fine to continue");
+        }
+
+        if ($("#avoPrice").val() ==''  ) {
+            console.log("no avocado price provided");
+            $(".error-avoprice").show() ;
+            $(".error-avoprice").css("display", "inline") ;
+            return 1 ;
+        } else {
+            console.log("Avocado price provided. continue as normal");
+            return 0 ;
+        }
+
+    }
+
+    /*
+     * After input validation errors show, we want to hide them when user clicks on input
+     */
+    $("#electorateSelector,  #avoPrice").on("click", function() {
+        $("span.error").fadeOut('fast') ;
+    }) ;
+    
 
     function updateStoryDetails () {
-	$(".avo-total-avos").text(avocadosTotal) ;
-	$(".avo-months").text(avocadosPerMonth) ;
-	$(".avo-week").text(avocadosPerWeek) ;
-	$(".avo-breakfast-freq-week").text(lostBreakfasts) ;
+        $(".avo-total-avos").text(avocadosTotal) ;
+        $(".avo-months").text(avocadosPerMonth) ;
+        $(".avo-week").text(avocadosPerWeek) ;
+        $(".avo-breakfast-freq-week").text(lostBreakfasts) ;
     } ;
 
     function showStory() {
-	var storyContainer = $(".story-container") ;
-	if (storyContainer.hasClass("hidden")) {
-	    storyContainer.fadeIn() ;
-	    storyContainer.removeClass("hidden") ;
-	}
-	
+        var storyContainer = $(".story-container") ;
+        if (storyContainer.hasClass("hidden")) {
+            storyContainer.fadeIn() ;
+            storyContainer.removeClass("hidden") ;
+        }
     } ;
 
     $("button.calculate").on("click", function(){
-        getValues() ;
-	updateStoryDetails() ;
-        showStory() ;
+        if ( avoInputValidation()  == 0) {
+            getValues() ;
+            updateStoryDetails() ;
+            showStory() ;
+        } else {
+            console.log("failed validation");
+        }
+
     });
 
 });
